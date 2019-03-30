f00ea4bcaa6a34941f9e84cffac1acf97bf2f29d
ikkei_kanaya <258lab@gmail.com>
2019-03-30 18:16:52 +0900

    ・モデルサンプル修正 ・AEモデルのチェック用プログラム作成

---
+++ autoencoder_cheker.py
@@ -0,0 +1,83 @@
+import autoencoder_model as autoencoder_model
+import sys, os
+from PIL import Image
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+
+# ----------------------------------------
+# AEモデルの検証用
+# ----------------------------------------
+image_size = 100
+
+# 顔認識用特徴量ファイルを読み込む --- （カスケードファイルのパスを指定）
+cascade = cv2.CascadeClassifier('./haarcascade_frontalface_alt2.xml')
+
+# 入力画像をnumpyに変換
+X = []
+files = []
+
+if __name__ == '__main__':
+    # ここは顔判断したい画像を指定
+    image_pic = cv2.imread("./g-0484-01.jpg")
+
+# # 顔認識の実行
+# face = cascade.detectMultiScale(image_pic)
+#
+# if len(face) == 0:
+#     print("顔検出できませんでした")
+#     exit()
+#
+# # 顔部分を切り取る
+# for x, y, w, h in face:
+#     face_cut = image_pic[y:y + h, x:x + w]
+#
+# cv2.imwrite('face_cut.jpg', face_cut)
+
+# 画像の読み込み
+img = Image.open("./g-0484-01.jpg")
+img = img.convert("RGB")
+img = img.resize((image_size, image_size))
+
+# 画像を表示
+# plt.imshow(img)
+# plt.show()
+
+in_data = np.asarray(img)
+X.append(in_data)
+
+X = np.array(X)
+
+# x_train, x_test, _, _ = np.load("./image/face3.npy")
+# x_train = x_train.astype('float32') / 255
+# x_test = x_test.astype('float32') / 255
+
+# モデル構築
+autoencoder = autoencoder_model.build_model(X.shape[1:])
+# autoencoder = autoencoder_model.build_model(x_train.shape[1:])
+autoencoder.load_weights('autoencoder.h5')
+
+# データを予測
+decoded_imgs = autoencoder.predict(X)
+
+# 入力画像と再現した画像を表示する
+n = 1  # 20個の画像を表示する
+
+plt.figure(figsize=(20, 4))
+for i in range(n):
+    # 特徴量を抽出する
+    # print('特徴量', y[i].shape)
+    # オリジナルのテスト画像を表示
+    ax = plt.subplot(2, n, i+1)
+    plt.imshow(X[i])
+    # plt.gray()
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+
+    # 変換された画像を表示
+    ax = plt.subplot(2, n, i+1+n)
+    plt.imshow(decoded_imgs[i])
+    # plt.gray()
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+plt.show()
--- autoencoder_model.py
+++ autoencoder_model.py
@@ -15,66 +15,170 @@ import numpy as np
 # x_train = x_train.reshape((len(x_train), np.prod(x_train.shape[1:])))
 # x_test = x_test.reshape((len(x_test), np.prod(x_test.shape[1:])))
 
-x_train, x_test, _, _ = np.load("./image/face3.npy")
-x_train = x_train.astype('float32') / 255
-x_test = x_test.astype('float32') / 255
-# x_train = x_train.reshape((len(x_train), np.prod(x_train.shape[1:])))
-# x_test = x_test.reshape((len(x_test), np.prod(x_test.shape[1:])))
+# x_train, x_test, _, _ = np.load("./image/face3.npy")
+# x_train = x_train.astype('float32') / 255
+# x_test = x_test.astype('float32') / 255
+# # x_train = x_train.reshape((len(x_train), np.prod(x_train.shape[1:])))
+# # x_test = x_test.reshape((len(x_test), np.prod(x_test.shape[1:])))
+#
+# # モデル構築
+# # input_img = Input(shape=(784,))
+# input_img = Input(shape=x_train.shape[1:])
+#
+# x = Convolution2D(64, 3, 3, activation='relu', border_mode='same')(input_img)
+# x = MaxPooling2D((2, 2), border_mode='same')(x)
+# x = Convolution2D(32, 3, 3, activation='relu', border_mode='same')(x)
+# x = MaxPooling2D((2, 2), border_mode='same')(x)
+# x = Convolution2D(16, 3, 3, activation='relu', border_mode='same')(x)
+# encoded = MaxPooling2D((2, 2), border_mode='same', name='encoder_layer')(x)
+#
+# x = Convolution2D(16, 3, 3, activation='relu', border_mode='same')(encoded)
+# x = UpSampling2D((2, 2))(x)
+# x = Convolution2D(32, 3, 3, activation='relu', border_mode='same')(x)
+# x = UpSampling2D((2, 2))(x)
+# x = Convolution2D(64, 3, 3, activation='relu')(x)
+# x = UpSampling2D((2, 2))(x)
+# decoded = Convolution2D(3, 3, 3, activation='sigmoid', border_mode='same')(x)
+#
+# autoencoder: Model = Model(input_img, decoded)
+# autoencoder.compile(optimizer='adam', loss='binary_crossentropy')
+#
+# # モデルの訓練
+# # autoencoder.fit(x_train, x_train, nb_epoch=10, batch_size=32, shuffle=True, validation_data=(x_test, x_test))
+#
+# # モデルの保存
+# # autoencoder.save_weights('autoencoder.h5')
+#
+# # モデルの読み込み
+# autoencoder.load_weights('autoencoder.h5')
+#
+# # 中間層の特徴量を抽出する
+# checkpoint_model = Model(autoencoder.input, autoencoder.get_layer(name='encoder_layer').output)
+# y = checkpoint_model.predict(x_test)
+# # print('特徴量', y.shape)
+#
+# decoded_imgs = autoencoder.predict(x_test)
+#
+# # 入力画像と再現した画像を表示する
+# n = 20  # 20個の画像を表示する
+#
+# plt.figure(figsize=(20, 4))
+# for i in range(n):
+#     # 特徴量を抽出する
+#     print('特徴量', y[i].shape)
+#     # オリジナルのテスト画像を表示
+#     ax = plt.subplot(2, n, i+1)
+#     plt.imshow(x_test[i])
+#     # plt.gray()
+#     ax.get_xaxis().set_visible(False)
+#     ax.get_yaxis().set_visible(False)
+#
+#     # 変換された画像を表示
+#     ax = plt.subplot(2, n, i+1+n)
+#     plt.imshow(decoded_imgs[i])
+#     # plt.gray()
+#     ax.get_xaxis().set_visible(False)
+#     ax.get_yaxis().set_visible(False)
+# plt.show()
 
-# モデル構築
-# input_img = Input(shape=(784,))
-input_img = Input(shape=x_train.shape[1:])
+# メイン処理
+def main():
+    # データ準備
+    # (x_train, _), (x_test, _) = mnist.load_data()
+    # x_train = x_train.astype('float32') / 255.
+    # x_test = x_test.astype('float32') / 255.
+    # x_train = x_train.reshape((len(x_train), np.prod(x_train.shape[1:])))
+    # x_test = x_test.reshape((len(x_test), np.prod(x_test.shape[1:])))
 
-x = Convolution2D(64, 3, 3, activation='relu', border_mode='same')(input_img)
-x = MaxPooling2D((2, 2), border_mode='same')(x)
-x = Convolution2D(32, 3, 3, activation='relu', border_mode='same')(x)
-x = MaxPooling2D((2, 2), border_mode='same')(x)
-x = Convolution2D(16, 3, 3, activation='relu', border_mode='same')(x)
-encoded = MaxPooling2D((2, 2), border_mode='same', name='encoder_layer')(x)
+    x_train, x_test, _, _ = np.load("./image/face3.npy")
+    x_train = x_train.astype('float32') / 255
+    x_test = x_test.astype('float32') / 255
 
-x = Convolution2D(16, 3, 3, activation='relu', border_mode='same')(encoded)
-x = UpSampling2D((2, 2))(x)
-x = Convolution2D(32, 3, 3, activation='relu', border_mode='same')(x)
-x = UpSampling2D((2, 2))(x)
-x = Convolution2D(64, 3, 3, activation='relu')(x)
-x = UpSampling2D((2, 2))(x)
-decoded = Convolution2D(3, 3, 3, activation='sigmoid', border_mode='same')(x)
+    # モデルを訓練
+    # autoencoder = train_model(x_train, x_test)
 
-autoencoder: Model = Model(input_img, decoded)
-autoencoder.compile(optimizer='adam', loss='binary_crossentropy')
+    # モデルを読み込む
+    autoencoder = load_model(x_test)
 
-# モデルの訓練
-# autoencoder.fit(x_train, x_train, nb_epoch=10, batch_size=32, shuffle=True, validation_data=(x_test, x_test))
+    # 中間層の特徴量を抽出する
+    checkpoint_model = Model(autoencoder.input, autoencoder.get_layer(name='encoder_layer').output)
+    y = checkpoint_model.predict(x_test)
+    # print('特徴量', y.shape)
 
-# モデルの保存
-# autoencoder.save_weights('autoencoder.h5')
+    decoded_imgs = autoencoder.predict(x_test)
 
-# モデルの読み込み
-autoencoder.load_weights('autoencoder.h5')
+    n = 10  # 10個の画像を表示する
 
-# 中間層の特徴量を抽出する
-checkpoint_model = Model(autoencoder.input, autoencoder.get_layer(name='encoder_layer').output)
-y = checkpoint_model.predict(x_test)
-print('特徴量', y.shape)
+    plt.figure(figsize=(20, 4))
+    for i in range(n):
+        # 特徴量を抽出す　→同じ数値しか取れない・・・
+        print('特徴量', y[i].shape)
+        # オリジナルのテスト画像を表示
+        ax = plt.subplot(2, n, i + 1)
+        plt.imshow(x_test[i])
+        # plt.gray()
+        ax.get_xaxis().set_visible(False)
+        ax.get_yaxis().set_visible(False)
 
-decoded_imgs = autoencoder.predict(x_test)
+        # 変換された画像を表示
+        ax = plt.subplot(2, n, i + 1 + n)
+        plt.imshow(decoded_imgs[i])
+        # plt.gray()
+        ax.get_xaxis().set_visible(False)
+        ax.get_yaxis().set_visible(False)
+    plt.show()
 
-# 入力画像と再現した画像を表示する
-n = 20  # 20個の画像を表示する
+# モデルの構築
+def build_model(in_shape):
+    input_img = Input(shape=in_shape)
 
-plt.figure(figsize=(20, 4))
-for i in range(n):
-    # オリジナルのテスト画像を表示
-    ax = plt.subplot(2, n, i+1)
-    plt.imshow(x_test[i])
-    # plt.gray()
-    ax.get_xaxis().set_visible(False)
-    ax.get_yaxis().set_visible(False)
+    x = Convolution2D(64, 3, 3, activation='relu', border_mode='same')(input_img)
+    x = MaxPooling2D((2, 2), border_mode='same')(x)
+    x = Convolution2D(32, 3, 3, activation='relu', border_mode='same')(x)
+    x = MaxPooling2D((2, 2), border_mode='same')(x)
+    x = Convolution2D(16, 3, 3, activation='relu', border_mode='same')(x)
+    encoded = MaxPooling2D((2, 2), border_mode='same', name='encoder_layer')(x)
 
-    # 変換された画像を表示
-    ax = plt.subplot(2, n, i+1+n)
-    plt.imshow(decoded_imgs[i])
-    # plt.gray()
-    ax.get_xaxis().set_visible(False)
-    ax.get_yaxis().set_visible(False)
-plt.show()
+    x = Convolution2D(16, 3, 3, activation='relu', border_mode='same')(encoded)
+    x = UpSampling2D((2, 2))(x)
+    x = Convolution2D(32, 3, 3, activation='relu', border_mode='same')(x)
+    x = UpSampling2D((2, 2))(x)
+    x = Convolution2D(64, 3, 3, activation='relu')(x)
+    x = UpSampling2D((2, 2))(x)
+    decoded = Convolution2D(3, 3, 3, activation='sigmoid', border_mode='same')(x)
+
+    autoencoder: Model = Model(input_img, decoded)
+    autoencoder.compile(optimizer='adam', loss='binary_crossentropy')
+
+    return autoencoder
+
+# モデルを訓練する
+def train_model(x_train, x_test):
+    # モデルの構築
+    autoencoder = build_model(x_train.shape[1:])
+    # モデルの訓練
+    autoencoder.fit(x_train, x_train, nb_epoch=10, batch_size=32, shuffle=True, validation_data=(x_test, x_test))
+    # モデルの保存
+    autoencoder.save_weights('autoencoder.h5')
+    return autoencoder
+
+# モデルを読み込む
+def load_model(x_train):
+    # モデルの構築
+    autoencoder = build_model(x_train.shape[1:])
+    # モデル読み込み
+    autoencoder.load_weights('autoencoder.h5')
+    return autoencoder
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
+
+
+
+
+
